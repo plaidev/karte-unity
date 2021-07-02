a3095b583367e328e9d17d27512a2f3f8754dc85
kota.fujiwara <kota.fujiwara@plaid.co.jp>
2021-07-02 18:07:21 +0900

    release 0.0.5

--- Unity/Assets/Editor/BuildProcess/PostBuildProcess.cs
+++ Unity/Assets/Editor/BuildProcess/PostBuildProcess.cs
@@ -12,13 +12,20 @@ public class PostBuildProcess : MonoBehaviour {
             PBXProject pj = new PBXProject ();
             pj.ReadFromString (File.ReadAllText (pjPath));
 
+#if UNITY_2019_3_OR_NEWER
             string targetName = pj.GetUnityMainTargetGuid();
-
+#else
+            string targetName = pj.TargetGuidByName("Unity-iPhone");
+#endif
             pj.AddBuildProperty (targetName, "OTHER_LDFLAGS", "-ObjC");
             pj.AddBuildProperty (targetName, "CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES", "YES");
             pj.AddBuildProperty (targetName, "ENABLE_BITCODE", "NO");
 
+#if UNITY_2019_3_OR_NEWER
             string unityFrameWorkTargetName = pj.GetUnityFrameworkTargetGuid();
+#else
+            string unityFrameWorkTargetName = pj.TargetGuidByName(PBXProject.GetUnityTargetName());
+#endif
             pj.AddBuildProperty (unityFrameWorkTargetName, "OTHER_CFLAGS", "-fmodules");
             pj.AddBuildProperty (unityFrameWorkTargetName, "OTHER_CFLAGS", "-fcxx-modules");
             pj.AddBuildProperty (unityFrameWorkTargetName, "ENABLE_BITCODE", "NO");
